86ebd08dd96203fee705465e62fef8e7b26a9a0c
Anh Vuong <vuonga1103@yahoo.com>
2020-06-08 20:07:05 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
 def reverse_each_word(sentence)
+  # .collect is the same as .map
+
+  # First split the sentence into an array of words
+  # Then use .collect or .map to generate a new array of words, where each word is reversed
+  # Then join the array of reversed words into a string with a space separating them
   sentence.split(" ").collect { |word| word.reverse }.join(" ")
 end
